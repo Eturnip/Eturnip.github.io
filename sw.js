e1b8a40dc0b06a379a1e15c190b80046167ddafa
Eturnip <35858753+Eturnip@users.noreply.github.com>
2024-04-30 10:39:09 -0600

    siteUpdate

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240429.122222';
+const cacheName = 'chirpy-20240430.103046';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
